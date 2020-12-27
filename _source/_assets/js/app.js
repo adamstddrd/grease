c4f976ede2c07790aed80d09b9d4e871ece53955
Adam Stoddard <adam@adamstoddard.com>
2020-12-26 16:42:57 -0800

    clean up unused code

--- _source/_assets/js/app.js
+++ _source/_assets/js/app.js
@@ -3,8 +3,6 @@ js entry point
 ---------------------------------------------------------------------------- */
 import * as scroll from './utilities/scroll.js';
 // import * as netlify from "./utilities/netlify.js";
-// import Turbolinks from "turbolinks";
-// Turbolinks.start();
 
 document.addEventListener('DOMContentLoaded', () => {
   scroll.animateOnScroll();
