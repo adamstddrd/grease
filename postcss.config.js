f0e489af67e84bd6be0d6ab3d54ab2ba64f5a155
Adam Stoddard <adam@adamstoddard.com>
2021-01-03 09:08:29 -0800

    unlimited, config-free css and js bundles

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = (ctx) => ({
     'postcss-import': {},
     'postcss-preset-env': {
       stage: 1,
-      importFrom: '_source/_assets/css/base/@root.css'
+      importFrom: '_source/_assets/css/_base/@root.css'
     },
     'cssnano': {},
   },
