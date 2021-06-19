e3fcf6d8cca1b96b6f815f018301abc9858ac11a
Adam Stoddard <adam@adamstoddard.com>
2021-06-19 10:12:39 -0700

    freshen

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,20 +1,22 @@
 const { build } = require('esbuild');
 const glob = require('fast-glob');
+
 let watchMode = false;
 
 process.argv.forEach((val) => {
-  if (val == '--watch') {
+  if (val === '--watch') {
     watchMode = true;
   }
 });
 
 (async () => {
-  let entryPoints = await glob('_source/_assets/**/[!_]*/*.js');
+  const entryPoints = await glob('_source/_assets/**/[!_]*/*.js');
   await build({
     entryPoints,
     bundle: true,
     minify: true,
     sourcemap: true,
+    splitting: true,
     watch: watchMode,
     format: 'esm',
     logLevel: 'info',
