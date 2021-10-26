29bd10e438550d5b4ca096df249ce8b97f4bdf8b
Adam Stoddard <adam@adamstoddard.com>
2021-10-26 10:24:54 -0700

    simplify AOS

--- _source/_assets/js/_utilities/aos.js
+++ _source/_assets/js/_utilities/aos.js
@@ -1,8 +1,7 @@
 /* ----------------------------------------------------------------------------
-trigger CSS animations on targeted elements when they enter the viewport
----------------------------------------------------------------------------- */
+ trigger CSS animations on targeted elements
+ ---------------------------------------------------------------------------- */
 
-// apply the animation when an observed element enters the viewport
 function animateElements(elements) {
   const observer = new IntersectionObserver((entries) => {
     let extraDelay = 0;
@@ -13,68 +12,45 @@ function animateElements(elements) {
         const animation = element.getAttribute('data-aos');
         const styles = getComputedStyle(element);
         const existingDelay = parseFloat(styles.animationDelay.slice(0, -1));
-        // add in any delay set via CSS
+        // add in any delay already set via CSS
         if (extraDelay > 0) {
           element.style.animationDelay = `${extraDelay + existingDelay}s`;
         }
         element.classList.add(animation);
-        extraDelay += 0.1;
+        extraDelay += 0.15;
         observer.unobserve(entry.target);
       }
     });
   });
 
-  // transposes parent animation settings to direct children
-  function prepChildAnimations(element) {
-    const elementAnimation = element.getAttribute('data-aos');
-    const elementStyles = getComputedStyle(element);
-    const elementDelay = parseFloat(elementStyles.animationDelay.slice(0, -1));
+  elements.forEach((element) => {
+    observer.observe(element);
+  });
+}
+
+// transposes parent animation settings to direct children
+function prepChildAnimations(elements) {
+  elements.forEach((element) => {
+    const animationClass = element.getAttribute('data-aos-children');
+    const styles = getComputedStyle(element);
+    const delay = parseFloat(styles.animationDelay.slice(0, -1));
+    const duration = parseFloat(styles.animationDuration.slice(0, -1));
     const childElements = element.querySelectorAll(':scope > *');
     childElements.forEach((child) => {
       const childElement = child;
       childElement.classList.add('animate');
-      childElement.setAttribute('data-aos', elementAnimation);
-      childElement.style.animationDelay = `${elementDelay}s`;
-      observer.observe(childElement);
+      childElement.setAttribute('data-aos', animationClass);
+      if (duration > 0) {
+        childElement.style.animationDuration = `${duration}s`;
+      }
+      if (delay > 0) {
+        childElement.style.animationDelay = `${delay}s`;
+      }
     });
-    element.classList.remove('animate');
-  }
-
-  // prep children for observation, or observe the element directly
-  elements.forEach((element) => {
-    if (element.hasAttribute('data-aos-children')) {
-      prepChildAnimations(element);
-    } else {
-      observer.observe(element);
-    }
   });
 }
 
-// use a mutation observer to apply behavior to new matching elements
-function observeNewElements() {
-  const options = {
-    childList: true,
-    attributes: true,
-    subtree: true,
-    attributeFilter: ['data-aos'],
-  };
-
-  function callback(mutations) {
-    mutations.forEach((mutation) => {
-      mutation.addedNodes.forEach((node) => {
-        if (node.tagName) { // elements only please
-          animateElements(node.querySelectorAll('[data-aos]'));
-        }
-      });
-    });
-  }
-
-  const observer = new MutationObserver(callback);
-  observer.observe(document.querySelector('body'), options);
-}
-
-// make it so
 export function animateOnScroll() {
+  prepChildAnimations(document.querySelectorAll('[data-aos-children]'));
   animateElements(document.querySelectorAll('[data-aos]'));
-  observeNewElements();
 }
