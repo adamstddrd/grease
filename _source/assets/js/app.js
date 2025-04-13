421f11d1d44d66c6bb3ffb405beeb82bea5434be
Adam Stoddard <adam@adamstoddard.com>
2025-04-13 12:19:51 -0700

    Centralize default block margins, add kitchen sink nav

--- _source/assets/js/app.js
+++ _source/assets/js/app.js
@@ -2,5 +2,6 @@
 js entry point
 ---------------------------------------------------------------------------- */
 import animateOnScroll from './_elements/animate-on-scroll.js';
+import detailsMenu from './_elements/details-menu.js';
 import RandomShapes from './_elements/random-shapes.js';
 import SmallDetails from './_elements/small-details.js';
