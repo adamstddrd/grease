e029fc37d60bef6ccfe67fe426324e3bd3449bb5
Adam Stoddard <adam@adamstoddard.com>
2026-05-09 20:34:03 -0700

    Add true modal, use CSS for scroll linked/triggered animations (#14)

--- _source/assets/js/app.js
+++ _source/assets/js/app.js
@@ -1,7 +1,6 @@
 /* ----------------------------------------------------------------------------
 js entry point
 ---------------------------------------------------------------------------- */
-import animateOnScroll from './_elements/animate-on-scroll.js';
 import PopoverMenu from './_elements/popover-menu.js';
 import RandomShapes from './_elements/random-shapes.js';
 import SmallDetails from './_elements/small-details.js';
