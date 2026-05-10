6a596e5e07e62db7f965d49ed879b17a359ab72d
Adam Stoddard <adam@adamstoddard.com>
2026-05-09 18:30:30 -0700

    Freshen

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,48 +4,46 @@ import lightingcss from './_source/_utilities/lightningcss.js';
 import image from './_source/_utilities/image.js';
 import style from './_source/_utilities/style.js';
 import setVar from './_source/_utilities/setVar.js';
-import fullDate from './_source/_utilities/fullDate.js';
 import markdownify from './_source/_utilities/markdownify.js';
 import { IdAttributePlugin } from '@11ty/eleventy';
 
 export default async function (eleventyConfig) {
-  /* --------------------------------------------------------------------------
+	/* --------------------------------------------------------------------------
   Plugins, bundles, shortcodes, filters
   -------------------------------------------------------------------------- */
-  eleventyConfig.addPlugin(esbuild);
-  eleventyConfig.addPlugin(lightingcss);
-  eleventyConfig.addPlugin(IdAttributePlugin);
-  eleventyConfig.addBundle('css', { transforms: [style] });
-  eleventyConfig.addShortcode('image', image);
-  eleventyConfig.addPairedShortcode('setVar', setVar);
-  eleventyConfig.addFilter('fullDate', fullDate);
-  eleventyConfig.addFilter('markdownify', markdownify);
+	eleventyConfig.addPlugin(esbuild);
+	eleventyConfig.addPlugin(lightingcss);
+	eleventyConfig.addPlugin(IdAttributePlugin);
+	eleventyConfig.addBundle('css', { transforms: [style] });
+	eleventyConfig.addShortcode('image', image);
+	eleventyConfig.addPairedShortcode('setVar', setVar);
+	eleventyConfig.addFilter('markdownify', markdownify);
 
-  /* --------------------------------------------------------------------------
+	/* --------------------------------------------------------------------------
   MarkdownIt settings
   -------------------------------------------------------------------------- */
-  const markdownItOptions = {
-    html: true,
-    typographer: true,
-  };
-  eleventyConfig.setLibrary('md', markdownIt(markdownItOptions));
+	const markdownItOptions = {
+		html: true,
+		typographer: true,
+	};
+	eleventyConfig.setLibrary('md', markdownIt(markdownItOptions));
 
-  /* --------------------------------------------------------------------------
+	/* --------------------------------------------------------------------------
   Files & folders
   -------------------------------------------------------------------------- */
-  eleventyConfig.ignores.add('.DS_Store');
-  eleventyConfig.setServerPassthroughCopyBehavior('passthrough');
-  eleventyConfig.addPassthroughCopy('_source/assets/fonts');
-  eleventyConfig.addPassthroughCopy('_source/assets/images');
+	eleventyConfig.ignores.add('.DS_Store');
+	eleventyConfig.setServerPassthroughCopyBehavior('passthrough');
+	eleventyConfig.addPassthroughCopy('_source/assets/fonts');
+	eleventyConfig.addPassthroughCopy('_source/assets/images');
 
-  return {
-    dir: {
-      input: '_source',
-      output: '_public',
-      layouts: '_layouts',
-      includes: '_includes',
-    },
-    templateFormats: ['html', 'md', 'liquid'],
-    htmlTemplateEngine: 'liquid',
-  };
+	return {
+		dir: {
+			input: '_source',
+			output: '_public',
+			layouts: '_layouts',
+			includes: '_includes',
+		},
+		templateFormats: ['html', 'md', 'liquid'],
+		htmlTemplateEngine: 'liquid',
+	};
 }
